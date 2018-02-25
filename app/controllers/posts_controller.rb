e5f5552e0f71f45d7378c4f58c669514b6d9f1b2
Manana101 <manana.jaworska@gmail.com>
2018-02-25 21:19:25 +0100

    add basic admin access

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, only: [:admin, :new, :create, :edit, :update, :destroy]
 
   def index
     if params[:category].blank?
@@ -43,6 +44,10 @@ class PostsController < ApplicationController
     redirect_to root_path, notice: "Post destroyed"
   end
 
+  def admin
+    redirect_to root_path if authenticate
+  end
+
   private
 
     def post_params
@@ -52,5 +57,15 @@ class PostsController < ApplicationController
     def find_post
       @post = Post.find(params[:id])
     end
+
+protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      admin_username = Rails.application.secrets.admin_username
+      admin_password = Rails.application.secrets.admin_password
+      username == admin_username && password == admin_password
+    end
+  end
 
 end
