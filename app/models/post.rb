5ad09e935b21406a5086af3748def3a24c635024
Manana101 <manana.jaworska@gmail.com>
2018-02-25 15:29:39 +0100

    carrierwave without S3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :category
   validates :title, :content, :category_id, presence: true
+
+  mount_uploader :image, ImageUploader
 end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# carrierwave doesn't work without requiring it here
+# TODO: investigate
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
