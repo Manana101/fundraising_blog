08b69b2116e942ad4252a46c49521382df09a7e8
Manana101 <manana.jaworska@gmail.com>
2018-03-04 14:15:23 +0100

    assets: precompile heroku fix

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ require 'carrierwave/orm/activerecord'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
 module OdbudowaDomku
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
